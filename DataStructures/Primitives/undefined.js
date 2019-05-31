b90ccc6b7876646a92c1a6101030e6485ee9691c
Rishi Patel <Rishi.Patel@LIBERTYMUTUAL.COM>
2019-05-31 11:53:10 -0400

    undefined

---
+++ DataStructures/Primitives/undefined.js
@@ -0,0 +1,9 @@
+let rishdawg;
+
+if( rishdawg === undefined) console.log('yo this not defined');
+
+/*
+  Describes when a variable or something is not been assigned a value
+  It comes from the global object.
+  It also not writeable, enumerable or configurable
+ */
